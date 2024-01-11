a66dffe72fd77e3a84deb589d708a0bb4fe88295
utkarsh anand <utkarshanand688@gmail.com>
2024-01-11 06:45:16 +0530

    chat Bot with Gemini

--- app_vision.py
+++ app_vision.py
@@ -8,30 +8,32 @@ from PIL import Image
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model=genai.GenerativeModel("gemini-pro-vision")
+model = genai.GenerativeModel("gemini-pro-vision")
 
-def get_gemini_response(input,image):
-    if input!="":
-        response=model.generate_content([input,image])
+def get_gemini_response(input_prompt, image):
+    if input_prompt != "" and image:
+        response = model.generate_content([input_prompt, image])
+    elif image:
+        response = model.generate_content(image)
     else:
-        response=model.generate_content(image)
+        response = "Please upload an image or provide an input prompt."
     return response.text
 
 
 st.set_page_config(page_title="Gemini Image Demo")
 
+st.header("Gemini Application")
+input_prompt = st.text_input("Input Prompt:", key="input")
 
-st.header("Gemini Appllication")
-input_text=st.text_input("Input Promt: ", Key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image = ""
 
-
-uploaded_file=st.file_uploader("Choose an image ...",type=["jpg","jpeg","png"])
-image=""
 if uploaded_file is not None:
-    image=Image.open(uploaded_file)
-    st.image (image,caption="Uploaded Image .",use_column_width=True)
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
-submit=st.button("Tell  me something")
+submit = st.button("Tell me something")
 if submit:
-    response=get_gemini_response(input_text,image)
+    response = get_gemini_response(input_prompt, image)
+    st.subheader("The prompt is here")
     st.write(response)
